65c239faac45029abce234ab5595c142843c4954
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 08:54:59 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,8 @@ def apply_clearance(cart)
   cart.each do |item, data| 
     data.each do |data_type, amount| 
       if cart[item][:clearance] == true
-        cart[item][:price] == (cart[item][:price])*(.8)
+        price = cart[item][:price]
+        cart[item][:price] == price * .8
       end 
     end 
   end 
