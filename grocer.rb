03b816a8f50099fec0f7b5c61ad6adc2e380a399
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 08:58:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ def apply_clearance(cart)
   # code here
   cart.each do |item, data| 
     if cart[item][:clearance] == true
-      price = cart[item][:price]*0.8
+      price = (cart[item][:price]*0.8)
       cart[item][:price] == price
     end 
   end 
